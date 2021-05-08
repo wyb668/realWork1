0620e9e45caace2a7f232ee9a3227b9bacccae5e
jack <jack@qq.com>
2021-05-08 14:25:21 +0800

    master commit

--- src/main/java/com/wyb/git/gitTest.java
+++ src/main/java/com/wyb/git/gitTest.java
@@ -6,5 +6,6 @@ public class gitTest {
         System.out.println("hello git");
         System.out.println("hello git3");
         System.out.println("hello hot-fix");
+        System.out.println("hello masterTest");
     }
 }
