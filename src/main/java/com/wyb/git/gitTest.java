04e10cbaf30b9a0dc0af000bf3f1b35d15b999b2
jack <jack@qq.com>
2021-05-08 13:30:16 +0800

    second commit

--- src/main/java/com/wyb/git/gitTest.java
+++ src/main/java/com/wyb/git/gitTest.java
@@ -3,5 +3,6 @@ package com.wyb.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
